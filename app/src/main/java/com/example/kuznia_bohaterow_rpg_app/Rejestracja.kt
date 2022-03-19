f4c3244740c11050d835cd6d7244760893593193
Immlerth <botolak.jurczyk@gmail.com>
2022-03-19 20:21:14 +0100

    Firebase - walidacja

--- app/src/main/java/com/example/kuznia_bohaterow_rpg_app/Rejestracja.kt
+++ app/src/main/java/com/example/kuznia_bohaterow_rpg_app/Rejestracja.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_rejestracja.*
 import java.lang.Exception
 
@@ -30,6 +31,9 @@ class Rejestracja : AppCompatActivity() {
 
         RButtonRejestracja.setOnClickListener{
             when{
+                TextUtils.isEmpty(editTextTextPersonName.text.toString()) -> {
+                    Toast.makeText(this,"Wprowadź nick",Toast.LENGTH_SHORT).show()
+                }
                 TextUtils.isEmpty(editTextTextEmailAddress2.text.toString()) -> {
                     Toast.makeText(this,"Wprowadź adres email",Toast.LENGTH_SHORT).show()
                 }
@@ -39,14 +43,32 @@ class Rejestracja : AppCompatActivity() {
                 TextUtils.isEmpty(editTextTextPassword3.text.toString()) -> {
                     Toast.makeText(this,"Wprowadź ponownie haslo",Toast.LENGTH_SHORT).show()
                 }
-
+                !editTextTextPassword2.text.toString().equals(editTextTextPassword3.text.toString()) -> {
+                    Toast.makeText(this,"Hasła nie są takie same",Toast.LENGTH_SHORT).show()
+                }
+                !checkBox.isChecked -> {
+                    Toast.makeText(this,"W celu utworzenia konta wymagana jest akceptacja regulaminu",Toast.LENGTH_SHORT).show()
+                }
+                editTextTextPassword2.text.toString().count()<7 -> {
+                    Toast.makeText(this,"Hasło powinno mieć więcej niż 6 znaków",Toast.LENGTH_SHORT).show()
+                }
+                editTextTextPassword2.text.toString().count()>128 -> {
+                    Toast.makeText(this,"Hasło jest zbyt długie",Toast.LENGTH_SHORT).show()
+                }
+                !android.util.Patterns.EMAIL_ADDRESS.matcher(editTextTextEmailAddress2.text.toString()).matches() -> {
+                    Toast.makeText(this,"Błędny adres email",Toast.LENGTH_SHORT).show()
+                }
                 else -> {
-
-
 
                     FirebaseAuth.getInstance().createUserWithEmailAndPassword(editTextTextEmailAddress2.text.toString(),editTextTextPassword2.text.toString()).addOnCompleteListener {
                             task -> if (task.isSuccessful) {
                             val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                        val db = FirebaseFirestore.getInstance()
+                        val user: MutableMap<String,String> = HashMap()
+                        user["nick"] = editTextTextPersonName.text.toString()
+                        user["id"] = firebaseUser.uid
+                        db.collection("users").add(user)
 
                             Toast.makeText(this,"Konto zostało założone w serwisie",Toast.LENGTH_SHORT).show()
 
@@ -54,6 +76,7 @@ class Rejestracja : AppCompatActivity() {
                             startActivity(MainIntent)
 
                     }else{
+
                             Toast.makeText(this,"Wystąpił błąd podczas tworzenia konta",Toast.LENGTH_SHORT).show()
                     }
                     }
