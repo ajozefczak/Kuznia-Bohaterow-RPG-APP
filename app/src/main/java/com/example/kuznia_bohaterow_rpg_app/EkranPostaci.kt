cdca2c97b32f7dcd8f268195e13b6c639d7401d3
Komp <kamil.w.ofi@gmail.com>
2022-03-20 00:15:22 +0100

    ŻECZY NA PRZYSZŁOŚĆ

---
+++ app/src/main/java/com/example/kuznia_bohaterow_rpg_app/EkranPostaci.kt
@@ -0,0 +1,11 @@
+package com.example.kuznia_bohaterow_rpg_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EkranPostaci : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ekran_postaci)
+    }
+}
