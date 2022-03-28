dcad82a041c4eb587edb508076808aeda20f154c
Komp <kamil.w.ofi@gmail.com>
2022-03-28 17:42:16 +0200

    v 0.1.5 Kalendarz

--- app/src/main/java/com/example/kuznia_bohaterow_rpg_app/EkranGracza.kt
+++ app/src/main/java/com/example/kuznia_bohaterow_rpg_app/EkranGracza.kt
@@ -3,16 +3,32 @@ package com.example.kuznia_bohaterow_rpg_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_ekran_gracza.*
 
 class EkranGracza : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ekran_gracza)
+        initListeners()
 
         GButtonPostac.setOnClickListener {
             val tworzeniePostaciIntent = Intent(this, TworzeniePostaci::class.java)
             startActivity(tworzeniePostaciIntent)
         }
     }
+
+    private fun initListeners() {
+        val GButtonKalendarz = findViewById<Button>(R.id.GButtonKalendarz)
+        GButtonKalendarz.setOnClickListener(GButtonKalendarzListener)
+    }
+
+    private val GButtonKalendarzListener = View.OnClickListener { callKalendarzActivity() }
+
+    private fun callKalendarzActivity() {
+        val KalendarzIntent = Intent(this, Kalendarz::class.java)
+        startActivity(KalendarzIntent)
+    }
+
 }
--- app/src/main/java/com/example/kuznia_bohaterow_rpg_app/Kalendarz.kt
+++ app/src/main/java/com/example/kuznia_bohaterow_rpg_app/Kalendarz.kt
@@ -1,11 +1,72 @@
 package com.example.kuznia_bohaterow_rpg_app
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.graphics.Color
+import android.text.TextUtils
+import android.text.method.LinkMovementMethod
+import android.util.Log
+import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_kalendarz.*
+import java.util.*
 
-class Kalendarz : AppCompatActivity() {
+
+class Kalendarz : AppCompatActivity(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    var day = 0
+    var month = 0
+    var year = 0
+    var hour = 0
+    var minute = 0
+
+    var savedDay = 0
+    var savedMonth = 0
+    var savedYear = 0
+    var savedHour = 0
+    var savedMinute = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kalendarz)
+        pickDate()
     }
+
+    private fun pickDate(){
+        KButtonData.setOnClickListener {
+            getDateTimeCalendar()
+
+            DatePickerDialog(this,this,year,month,day).show()
+        }
+    }
+
+    private fun getDateTimeCalendar(){
+        val cal: Calendar = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        month = cal.get(Calendar.MONTH)
+        year = cal.get(Calendar.YEAR)
+        hour = cal.get(Calendar.HOUR)
+        minute = cal.get(Calendar.MINUTE)
+    }
+
+    override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+        savedDay = p3
+        savedMonth = p2
+        savedYear = p1
+
+        getDateTimeCalendar()
+        TimePickerDialog(this,this,hour,minute,true).show()
+    }
+
+    override fun onTimeSet(p0: TimePicker?, p1: Int, p2: Int) {
+        savedHour = p1
+        savedMinute = p2
+
+        KTextPoka.text ="$savedDay-$savedMonth-$savedYear\n Hour: $savedHour Minuty: $savedMinute"
+    }
+
+
 }
