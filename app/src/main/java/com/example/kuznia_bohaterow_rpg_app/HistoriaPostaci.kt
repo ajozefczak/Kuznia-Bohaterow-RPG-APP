23bd4e9dfb69b59e1d5e1895f3fd1cd170751097
Komp <kamil.w.ofi@gmail.com>
2022-04-13 21:24:06 +0200

    v 0.3.9 aktywnosci

---
+++ app/src/main/java/com/example/kuznia_bohaterow_rpg_app/HistoriaPostaci.kt
@@ -0,0 +1,11 @@
+package com.example.kuznia_bohaterow_rpg_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistoriaPostaci : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historia_postaci)
+    }
+}
