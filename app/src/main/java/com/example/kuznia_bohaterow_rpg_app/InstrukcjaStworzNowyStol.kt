b904a850554ea77ecb6a46d8bff01ffb5b8305d6
Komp <kamil.w.ofi@gmail.com>
2022-06-05 16:11:06 +0200

    v 0.8.8 instrukcja

---
+++ app/src/main/java/com/example/kuznia_bohaterow_rpg_app/InstrukcjaStworzNowyStol.kt
@@ -0,0 +1,11 @@
+package com.example.kuznia_bohaterow_rpg_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InstrukcjaStworzNowyStol : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instrukcja_stworz_nowy_stol)
+    }
+}
